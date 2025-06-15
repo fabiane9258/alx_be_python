7adee8e4dabb1e760143cff5690bac7434471ba4
fabiane9258 <fabiane9258@gmail.com>
2025-06-15 14:34:13 +0300

    Add BankAccount class and main script for basic banking operations

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    def __init__(self, initial_balance=0.0):
+        self.__account_balance = float(initial_balance)
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+        else:
+            raise ValueError("Deposit aount must be positive. ")
+    
+    def withdraw(self, amount):
+        if amount > 0:
+            if amount <= self.__account_balance:
+                self.__account_balance -= amount
+                return True
+            else:
+                return False
+        else:
+            raise ValueError("Withdrawal amount must be positive. ")
+        
+    def display_balance(self):
+        print(f"Current balance: ${self.__account_balance:.2f}")
+        
